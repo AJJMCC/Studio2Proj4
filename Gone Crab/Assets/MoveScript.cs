24950dacc0e72270e52ed8cd0ac9cf150d175c0a
Caleb Middlebrook <1005802@student.sae.edu.au>
2017-03-30 22:49:40 +1000

    Worked on the level design scene / terrain.

---
+++ Gone Crab/Assets/MoveScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetAxis("Vertical") != 0)
+        {
+            float speed = moveSpeed * Input.GetAxis("Vertical");
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float speed = moveSpeed * Input.GetAxis("Horizontal");
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+    }
+}
