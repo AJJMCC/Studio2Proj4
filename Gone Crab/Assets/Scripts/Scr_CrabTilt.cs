8b2d7597f97821d7147432ed59b90ea5893b8e1b
Dakota Wood <dwood32@sae.au>
2017-04-03 17:33:10 +1000

    Crab Movement and Tilting completed to a functional level. Still needs polish later. Camera and relative rotation is up next.

---
+++ Gone Crab/Assets/Scripts/Scr_CrabTilt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_CrabTilt : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 backLeft;
+    [SerializeField]
+    private Vector3 backRight;
+    [SerializeField]
+    private Vector3 frontLeft;
+    [SerializeField]
+    private Vector3 frontRight;
+
+    private RaycastHit lr;
+    private RaycastHit rr;
+    private RaycastHit lf;
+    private RaycastHit rf;
+
+    private Vector3 upDir;
+
+    void Update()
+    {
+        Vector3 v = this.gameObject.transform.position;
+
+        Physics.Raycast(v + backLeft, Vector3.down, out lr);
+        Physics.Raycast(v + backRight, Vector3.down, out rr);
+        Physics.Raycast(v + frontLeft, Vector3.down, out lf);
+        Physics.Raycast(v + frontRight, Vector3.down, out rf);
+
+        upDir = (Vector3.Cross(rr.point - Vector3.up, lr.point - Vector3.up) +
+                 Vector3.Cross(lr.point - Vector3.up, lf.point - Vector3.up) +
+                 Vector3.Cross(lf.point - Vector3.up, rf.point - Vector3.up) +
+                 Vector3.Cross(rf.point - Vector3.up, rr.point - Vector3.up)
+                ).normalized;
+
+        transform.up = upDir;
+
+        Debug.DrawRay(rr.point, Vector3.up, Color.white, 1000);
+        Debug.DrawRay(lr.point, Vector3.up, Color.white, 1000);
+        Debug.DrawRay(lf.point, Vector3.up, Color.white, 1000);
+        Debug.DrawRay(rf.point, Vector3.up, Color.white, 1000);
+    }
+}
--- Gone Crab/Assets/Scripts/Scr_PlayerCrab.cs
+++ Gone Crab/Assets/Scripts/Scr_PlayerCrab.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Scr_PlayerCrab : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    #region Private References
+    private Rigidbody rb;
+    #endregion
+
+    #region Serialized Fields
+    [SerializeField]
+    private float groundSpeed = 10.0f;
+    [SerializeField]
+    public float maxVelocity = 50.0f;
+    #endregion
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = this.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
 	}
+
+    void FixedUpdate()
+    {
+        // Call Control function
+        Control();
+    }
+
+    // Updates the position of the crab by taking input from the player
+    void Control()
+    {
+        Vector3 ControlVector = new Vector3(0,0,0);
+        // TODO: Get this infor from our Input Script (P = 0)
+        ControlVector.x = Input.GetAxis("Vertical");
+        ControlVector.z = Input.GetAxis("Horizontal") * -1.0f;
+
+        // Add Input Movement
+        rb.MovePosition(rb.position + (ControlVector * groundSpeed * Time.deltaTime));
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    }
 }
