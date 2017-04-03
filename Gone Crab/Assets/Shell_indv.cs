274767ac259cf4b1b4312bcae38e222b3ce8a5c7
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-04-03 18:07:04 +1000

    new scene and script for jacks shell shit on tuesday

---
+++ Gone Crab/Assets/Shell_indv.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell_indv : MonoBehaviour {
+
+    private float Mintight = 0.7f;
+
+    private float Minavg = 0.9f;
+
+    private float MinSpac = 1.1f;
+
+    private float Maxspac = 1.3f;
+
+
+   // public bool isAcceptable;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public bool isAcceptable()
+    {
+        if (true)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
