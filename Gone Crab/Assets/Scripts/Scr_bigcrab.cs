55044b210c0c9eb929d2fa5c1b391c17331ccfef
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-04-12 19:36:38 +1000

    added big crab model and prefab with animator attached, and script, is ready to drag and drop.

---
+++ Gone Crab/Assets/Scripts/Scr_bigcrab.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_bigcrab : MonoBehaviour {
+
+    private Animator thisanim;
+
+    private GameObject playercrab;
+
+    private float Pdist;
+
+    private bool toldtorun;
+    private bool running = false;
+    private Vector3 movetopos; 
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        playercrab = GameObject.FindGameObjectWithTag("Player");
+        thisanim = this.GetComponent<Animator>();
+        Vector3 movetopos = new Vector3(300, 150, -400);
+	}
+	
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        Pdist = Vector3.Distance(this.transform.position, playercrab.transform.position);
+
+        if (Pdist <= 25 && playercrab.transform.localScale.x < 11)
+        {
+            thisanim.SetTrigger("Raise claws");
+            Debug.Log("raised claws");
+        }
+
+        else if (Pdist <= 30 && playercrab.transform.localScale.x >= 11)
+        {
+            if (!toldtorun)
+            {
+                Run();
+            }
+           
+        }
+        if (running == true)
+        {
+            transform.Rotate(1  , 1, 1 );
+            transform.position += Vector3.up;
+        }
+
+    }
+
+    private void Run()
+    {
+        Destroy(gameObject, 3);
+        toldtorun = true;
+        running = true;
+    }
+}
