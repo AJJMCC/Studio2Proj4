cb5182d8893fd10aacd21d4e1cff6d3085404f9e
Caleb Middlebrook <1005802@student.sae.edu.au>
2017-04-12 19:57:09 +1000

    Modified beach level (invisible barriers and cliff rocks) and added an interactable beach ball (wip)

---
+++ Gone Crab/Assets/Scripts/Scr_Interactable.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Interactable : MonoBehaviour
+{
+    public float minSize;
+    public float maxSize;
+
+    public float size;
+
+    private float Pdistance;
+    public float ScaleActiveDist;
+    private float ActiveDistance;
+    private GameObject player;
+
+    [SerializeField]
+    private Renderer rend;
+
+    public Color purple;
+    public Color red;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ActiveDistance = ScaleActiveDist * player.transform.localScale.x;
+
+        ChecksAgainstPlayer();
+        PrettyLights();
+    }
+
+    private void ChecksAgainstPlayer()
+    {
+        Pdistance = Vector3.Distance(this.transform.position, player.transform.position);
+
+        size = this.transform.localScale.x / player.transform.localScale.x;
+    }
+
+    public bool isAcceptable()
+    {
+        string shellV = ShellState();
+        if (shellV == "UNACCEPTABLE")
+        {
+            return false;
+        }
+        else
+            return true;
+    }
+
+
+    public string ShellState()
+    {
+        if (size <= maxSize && size >= minSize)
+        {
+            return "Interactable";
+        }
+        else
+            return "UNACCEPTABLE";
+    }
+
+
+    private void PrettyLights()
+    {
+
+        if (Pdistance >= ActiveDistance)
+        {
+            rend.material.SetColor("_Color", Color.white);
+        }
+
+        string colourcheck = ShellState();
+
+        if (Pdistance <= ActiveDistance)
+        {
+            if (colourcheck == "Interactable")
+            {
+                rend.material.SetColor("_Color", purple);
+            }
+            else
+                rend.material.SetColor("_Color", red);
+        }
+    }
+}
