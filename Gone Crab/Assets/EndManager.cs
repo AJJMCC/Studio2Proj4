c162418cf4c1d7581d2ba71beec05151e892ff75
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-04-18 14:49:05 +1000

    shit added

---
+++ Gone Crab/Assets/EndManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void RePlay()
+    {
+        Application.LoadLevel("leveldesign");
+
+    }
+
+
+    public void ToMenu()
+    {
+        Application.LoadLevel("mainmenu");
+    }
+}
