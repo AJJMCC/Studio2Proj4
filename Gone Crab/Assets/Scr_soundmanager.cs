660d29d1eb21a9dd4b654873d88f521c59df446e
Caleb Middlebrook <1005802@student.sae.edu.au>
2017-04-20 15:20:19 +1000

    Added collab sounds and did the thing

--- Gone Crab/Assets/Scr_soundmanager.cs
+++ Gone Crab/Assets/Scr_soundmanager.cs
@@ -7,7 +7,10 @@ public class Scr_soundmanager : MonoBehaviour {
     public AudioSource VFX;
     public AudioClip shelloff;
     public AudioClip shellon;
-    public AudioClip SizzleLoop; 
+    public AudioClip SizzleLoop;
+    public AudioClip fallingSound;
+    public AudioClip sandWalk;
+    public AudioClip rockWalk;
 
     //Use this for initialization
 	void Start ()
@@ -18,16 +21,51 @@ public class Scr_soundmanager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		
+		if (Input.GetKeyDown("w"))
+        {
+            ChangeWalkClip(0);
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            ChangeWalkClip(1);
+        }
 	}
 
+    // Plays the shell on clip
     public void ShellAdded()
     {
         VFX.PlayOneShot(shellon);
     }
 
+    // Plays the shell off clip
     public void ShellPopped()
     {
         VFX.PlayOneShot(shelloff);
+    }
+
+    // Plays the falling sound clip
+    public void Falling()
+    {
+        VFX.PlayOneShot(fallingSound);
+    }
+
+    // Checks what the current clip is and changes it accordingly
+    public void ChangeWalkClip(int walkType)
+    {
+        if (walkType == 0)
+        {
+            VFX.clip = rockWalk;
+            VFX.Play();
+        }
+        else
+        {
+            VFX.clip = sandWalk;
+            VFX.Play();
+        }
+    }
+
+    public void WalkVolume(float walkSpeed)
+    {
+        VFX.volume = walkSpeed;
     }
 }
