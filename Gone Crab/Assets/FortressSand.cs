bd18a07a0244721cccdf345cdb94c0e56d519c13
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-04-24 17:24:14 +1000

    Sand fortress added for testing in main level

---
+++ Gone Crab/Assets/FortressSand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FortressSand : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.collider.tag == "Player")
+        {
+            if (collider.transform.localScale.x >= this.transform.localScale.x/2)
+            {
+                this.GetComponent<QuickStart>().enabled = true;
+            }
+        }
+
+
+    }
+}
